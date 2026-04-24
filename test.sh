a38826eca63d9c1de696a7113b352d2534aa2a5e
Alan Layng <alan.layng@gmail.com>
2026-04-24 19:06:44 +0100

    Ensure test script doesn't use an init file

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ IFS=$'\t\n'; set -euo pipefail
 
 cd "${0%/*}"
 CFLAGS="-DDEBUG_TRACE -DDEBUG_ASSERT" make clean all || { echo "rc=$?"; exit 1; }
-sqlite3 <test_script.sql &>test.actual || true
+sqlite3 --noinit <test_script.sql &>test.actual || true
 diff -C 5 test.{actual,expected} || exit 1
 echo "-----------"
 echo "Test passed"
